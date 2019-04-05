2c88f0cc6ceac7f63e9024d1f255466f853bc02d
Aaron L. Zeng <me@bcc32.com>
2019-04-05 15:03:35 -0400

    Add -download-dir flag

--- src/cmd_download.ml
+++ src/cmd_download.ml
@@ -2,7 +2,8 @@ open! Core
 open! Async
 open! Import
 
-let main dbpath =
+let main dbpath ~download_dir =
+  let%bind () = Unix.chdir download_dir in
   let deferreds = ref [] in
   let db = Db.open_file dbpath in
   Db.iter_non_watched_videos db ~f:(fun video ->
@@ -14,6 +15,13 @@ let command =
   Command.async
     ~summary:"Download youtube videos from database"
     (let%map_open.Command.Let_syntax () = return ()
-     and dbpath = anon ("DBPATH" %: Filename.arg_type) in
-     fun () -> main dbpath)
+     and dbpath = anon ("DBPATH" %: Filename.arg_type)
+     and download_dir =
+       flag
+         "download-dir"
+         (required Filename.arg_type)
+         ~doc:"DIR directory to store downloads in"
+         ~aliases:[ "d" ]
+     in
+     fun () -> main dbpath ~download_dir)
 ;;
--- src/download.ml
+++ src/download.ml
@@ -5,7 +5,7 @@ open! Import
 let throttle = Throttle.create ~continue_on_error:false ~max_concurrent_jobs
 
 let download (video : Video.t) =
-  let working_dir = "download" ^/ video.channel_id ^ "-" ^ video.channel_title in
+  let working_dir = video.channel_id ^ "-" ^ video.channel_title in
   let f () =
     Async_interactive.Job.run !"downloading %{sexp:Video.t}" video ~f:(fun () ->
       let%bind () = Unix.mkdir ~p:() working_dir in
