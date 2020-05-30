b04d3d0e62fb7684b6949d8ce7c49462781e4a55
Aaron L. Zeng <me@bcc32.com>
2020-05-30 15:55:46 -0400

    Build using nix

---
+++ default.nix
@@ -0,0 +1,25 @@
+with import <nixpkgs> { };
+
+let
+  inherit (ocamlPackages)
+    buildDunePackage async async_interactive async_ssl cohttp-async core
+    ocaml_sqlite3 uri webbrowser yojson;
+
+in buildDunePackage {
+  pname = "watch-later";
+  version = "0.1.0";
+  useDune2 = true;
+  src = lib.cleanSource ./.;
+  propagatedBuildInputs = [
+    async
+    async_interactive
+    async_ssl
+    cohttp-async
+    core
+    ocaml_sqlite3
+    uri
+    webbrowser
+    yojson
+  ];
+  meta = { homepage = "https://github.com/bcc32/watch-later"; };
+}
