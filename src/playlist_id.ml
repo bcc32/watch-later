6a81bc02cc59422c941505cf03af8e8f42fe1f24
Aaron L. Zeng <me@bcc32.com>
2021-01-21 03:31:56 -0500

    ocamlformat

--- src/playlist_id.ml
+++ src/playlist_id.ml
@@ -9,17 +9,17 @@ include String_id.Make
     ()
 
 module Plain_or_in_url = struct
-let of_url uri =
-match List.Assoc.find (Uri.query uri) "list" ~equal:String.equal with
-| Some [playlist_id] -> of_string playlist_id
-| Some list -> raise_s [%message "invalid list= query parameter" (list : string list)]
-| None ->
-raise_s [%message "missing list= query parameter" ~url:(Uri.to_string uri)]
-;;
-let of_string string =
-try string |> Uri.of_string |> of_url with
-| _ -> of_string string
-;;
+  let of_url uri =
+    match List.Assoc.find (Uri.query uri) "list" ~equal:String.equal with
+    | Some [ playlist_id ] -> of_string playlist_id
+    | Some list -> raise_s [%message "invalid list= query parameter" (list : string list)]
+    | None -> raise_s [%message "missing list= query parameter" ~url:(Uri.to_string uri)]
+  ;;
 
-let arg_type = Command.Arg_type.create of_string
+  let of_string string =
+    try string |> Uri.of_string |> of_url with
+    | _ -> of_string string
+  ;;
+
+  let arg_type = Command.Arg_type.create of_string
 end
--- src/playlist_id.mli
+++ src/playlist_id.mli
@@ -3,7 +3,6 @@ open! Async
 open! Import
 include String_id.S
 
-
 module Plain_or_in_url : sig
   val of_string : string -> t
   val arg_type : t Command.Arg_type.t
