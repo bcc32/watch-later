0deac49a2177e51c80dab3d454b41fc4b3571cd3
Aaron L. Zeng <me@bcc32.com>
2021-02-16 00:00:53 -0500

    Batch video info requests in add command

--- src/cmd_add.ml
+++ src/cmd_add.ml
@@ -4,18 +4,27 @@ open! Import
 
 let main ~api ~dbpath ~mark_watched ~overwrite ~video_ids =
   Video_db.with_file_and_txn dbpath ~f:(fun db ->
-    Deferred.List.map video_ids ~f:(fun video_id ->
-      (* FIXME: This check/add should be in a transaction. *)
-      let%bind already_present = Video_db.mem db video_id in
-      if already_present && not overwrite
-      then (
-        match mark_watched with
-        | None -> return ()
-        | Some state -> Video_db.mark_watched db video_id state)
-      else (
-        let%bind video_info = Youtube_api.get_video_info api video_id in
-        Video_db.add_video db video_info ~mark_watched ~overwrite))
-    |> Deferred.map ~f:Or_error.combine_errors_unit)
+    video_ids
+    |> Pipe.of_list
+    |> Youtube_api.get_video_info' api
+    |> Pipe.fold ~init:[] ~f:(fun accum video_info ->
+      let result =
+        let%bind video_info = Deferred.return video_info in
+        let%bind already_present = Video_db.mem db video_info.video_id in
+        if already_present && not overwrite
+        then (
+          match mark_watched with
+          | None -> return ()
+          | Some state -> Video_db.mark_watched db video_info.video_id state)
+        else (
+          let%bind video_info =
+            Youtube_api.get_video_info api video_info.video_id
+          in
+          Video_db.add_video db video_info ~mark_watched ~overwrite)
+      in
+      let%map.Deferred result = result in
+      result :: accum)
+    |> Deferred.map ~f:(Or_error.combine_errors_unit << List.rev))
 ;;
 
 let command =
--- src/import.ml
+++ src/import.ml
@@ -1,4 +1,5 @@
 open! Core
 open! Async
+include Composition_infix
 include Ppx_log_async
 include Deferred.Or_error.Let_syntax
