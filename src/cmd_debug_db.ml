b8e200913c965827d217483342ee3c8345191006
Aaron L. Zeng <me@bcc32.com>
2021-02-18 14:44:33 -0500

    Add `debug db query` subcommand

--- src/cmd_debug_db.ml
+++ src/cmd_debug_db.ml
@@ -14,6 +14,34 @@ module Print_path = struct
   ;;
 end
 
+module Query = struct
+  let make_safe_path file = if Filename.is_implicit file then "./" ^ file else file
+
+  let command =
+    Command.basic
+      ~summary:"Run an arbitrary SQL query on stdin against the database"
+      (let%map_open.Command () = return ()
+       and dbpath = Params.dbpath
+       and args =
+         flag "--" escape ~doc:"ARG command-line arguments to pass to sqlite3"
+         >>| Option.value ~default:[]
+       in
+       fun () ->
+         (* Caqti doesn't support ['a Caqti_type.t]s that read unknown numbers of fields,
+            so we just shell out to the sqlite3 command-line shell instead. *)
+         never_returns
+           (Core.Unix.exec
+              ~prog:"sqlite3"
+              ~argv:
+                ([ "sqlite3"; "-batch"; "-bail"; "-init"; "/dev/null" ]
+                 @ args
+                 @ [ make_safe_path dbpath ])
+              ()))
+  ;;
+end
+
 let command =
-  Command.group ~summary:"Database debugging commands" [ "path", Print_path.command ]
+  Command.group
+    ~summary:"Database debugging commands"
+    [ "path", Print_path.command; "query", Query.command ]
 ;;
