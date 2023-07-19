2e8ec5f726c17dc84e01e9d9719d5b5c33ce2878
Aaron L. Zeng <me@bcc32.com>
2023-07-18 23:15:30 -0400

    Build on multiple systems using flake-utils

--- flake.nix
+++ flake.nix
@@ -1,53 +1,55 @@
 {
   description = "Utility for managing a queue of YouTube content";
 
-  inputs = { };
+  inputs = { flake-utils.url = "github:numtide/flake-utils"; };
 
-  outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in with pkgs; rec {
-      devShells.x86_64-linux.default = mkShell {
-        inputsFrom = [ packages.x86_64-linux.default ];
-        buildInputs = packages.x86_64-linux.default.checkInputs ++ [
-          inotify-tools
-          ocamlPackages.merlin
-          ocamlformat
-          ocamlPackages.ocp-indent
-          ocamlPackages.utop
-        ];
-      };
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in with pkgs; rec {
+        devShells.default = mkShell {
+          inputsFrom = [ packages.default ];
+          buildInputs = packages.default.checkInputs ++ [
+            inotify-tools
+            ocamlPackages.merlin
+            ocamlformat
+            ocamlPackages.ocp-indent
+            ocamlPackages.utop
+          ];
+        };
 
-      packages.x86_64-linux.default = ocamlPackages.buildDunePackage rec {
-        pname = "watch-later";
-        version = "0.1.0";
-        duneVersion = "3";
-        src = nix-gitignore.gitignoreFilterSource lib.cleanSourceFilter [ ] ./.;
-        checkInputs = with ocamlPackages; [ shexp ];
-        propagatedBuildInputs = with ocamlPackages; [
-          async
-          async_interactive
-          async_ssl
-          caqti-async
-          caqti-driver-sqlite3
-          cohttp-async
-          core
-          cryptokit
-          directories
-          fzf
-          jsonaf
-          ppx_jsonaf_conv
-          ppx_log
-          uri
-          webbrowser
-        ];
-        passthru.checkInputs = checkInputs;
+        packages.default = ocamlPackages.buildDunePackage rec {
+          pname = "watch-later";
+          version = "0.1.0";
+          duneVersion = "3";
+          src =
+            nix-gitignore.gitignoreFilterSource lib.cleanSourceFilter [ ] ./.;
+          checkInputs = with ocamlPackages; [ shexp ];
+          propagatedBuildInputs = with ocamlPackages; [
+            async
+            async_interactive
+            async_ssl
+            caqti-async
+            caqti-driver-sqlite3
+            cohttp-async
+            core
+            cryptokit
+            directories
+            fzf
+            jsonaf
+            ppx_jsonaf_conv
+            ppx_log
+            uri
+            webbrowser
+          ];
+          passthru.checkInputs = checkInputs;
 
-        nativeBuildInputs = [ installShellFiles ];
-        postInstall = ''
-          installShellCompletion share/completions/wl.bash
-        '';
+          nativeBuildInputs = [ installShellFiles ];
+          postInstall = ''
+            installShellCompletion share/completions/wl.bash
+          '';
 
-        meta = { homepage = "https://github.com/bcc32/watch-later"; };
-      };
-    };
+          meta = { homepage = "https://github.com/bcc32/watch-later"; };
+        };
+      });
 }
